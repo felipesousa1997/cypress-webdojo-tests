4b575e1d7f4cdc0c371fe91f037363ed1ba226db
Felipe Sousa <fsousa034@gmail.com>
2026-09-07 10:02:08 -0300

    refactor: permite login via UI ou estado da aplicação
    Adiciona a opção de autenticar diretamente via cookie e localStorage, mantendo o login pela interface disponível através do parâmetro ui.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -49,7 +49,22 @@ Cypress.Commands.add('goTo', (buttonName, pageTitle) => {
 })
 
 // Helper
-Cypress.Commands.add('login', () => {
+Cypress.Commands.add('login', (ui = false) => {
+
+    if (ui === true) {
         cy.start()
         cy.submitLoginForm('papito@webdojo.com', 'katana123')
+    }
+
+    else {
+        const token = 'e1033d63a53fe66c0fd3451c7fd8f617'
+        const loginDate = getTodayDate()
+
+        cy.setCookie('login_date', loginDate)
+        cy.visit('http://localhost:3000/dashboard', {
+            onBeforeLoad(win) {
+            win.localStorage.setItem('token', token)
+            }
+        })
+    }
 })
