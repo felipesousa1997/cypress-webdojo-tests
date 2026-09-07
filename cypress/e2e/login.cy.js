70cfa06ef07c02aa65c877585b16ec1e60a9316d
Felipe Sousa <fsousa034@gmail.com>
2026-09-07 12:39:57 -0300

    refactor: centraliza função de data atual em utils
    Move getTodayDate to utils.js for reuse in commands and login tests.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,6 @@
+import {getTodayDate} from "../support/utils.js"
+
 describe('Login', () => {
-
-  function getTodayDate() {
-      const today = new Date()
-
-      const day = String(today.getDate()).padStart(2, '0')
-      const month = String(today.getMonth() + 1).padStart(2, '0')
-      const year = today.getFullYear()
-
-      return `${day}/${month}/${year}`
-    }
 
   it('Deve logar com sucesso!', () => {
       cy.start()
@@ -52,4 +44,3 @@ describe('Login', () => {
 
     })
 })
-
